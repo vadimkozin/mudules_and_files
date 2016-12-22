0bc2dfbcae6cb805a122af1f961f657de8c2b14d
Vadim Kozin <ulin.salma@yandex.ru>
2016-12-22 21:52:44 +0300

    переписал hide

--- hidenseek.js
+++ hidenseek.js
@@ -11,42 +11,6 @@ const file = 'pokemon.txt';
 const prefix = 'data-';
 const countDir = 10;
 
-function hide_(path, pokemonList) {
-  return new Promise((resolve,reject) => {
-    // нужно спрятать не более 3 и не более чем передано
-    let  hideCount = Math.min(3,  pokemonList.length);
-    // покемоны должны быть выбраны из списка случайным образом
-    const hideList = randomList(0, pokemonList.length - 1, hideCount);
-    // 10 папок
-    const dirs = [];
-    for (let i = 0; i < countDir; i++) {
-      dirs.push(`${path}/${prefix}${twoDigits(i)}`);
-    }
-    // случайный список директорий для сохранения инфо о пикемоне
-    const indexDir = randomList(0, countDir - 1, hideCount);
-
-    let n = 0;
-    let chain = Promise.resolve();
-    dirs.forEach( (dir, i) => {
-      chain = chain
-      .then(() => emptyDir(dir))
-      .then((result) => { 
-        if (indexDir.indexOf(i) >- 1) {
-          createFile(`${dir}/${file}`, pokemonList[hideList[n++]].info());
-          return  `${dir}/${file}`;
-        }
-        return result;
-      })
-      //.then(result => console.log(result))
-      .catch(err => { throw err });
-    });
-
-    resolve(pokemonList.filter((x, i) => { 
-      return (hideList.indexOf(i) > -1)
-    }));
-
-  });
-}
 
 function hide(path, pokemonList) {
   return new Promise((resolve,reject) => {
